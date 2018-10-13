cea718722e09b8a63e9e1f10d5019e0b67475a5b
Joe Fehrman <joseph.fehrman@gmail.com>
2018-10-13 15:58:42 -0400

    Added all of the project dependencies and configured the project.

---
+++ .storybook/config.js
@@ -0,0 +1,22 @@
+import { configure } from '@storybook/react';
+import '../src/stories/index.css';
+
+const req = require.context('../src/components', true, /\.story\.js$/);
+
+const sortByFileName = (filePath1, filePath2) => {
+  const file1 = filePath1.split('\\').pop().split('/').pop();;
+  const file2 = filePath2.split('\\').pop().split('/').pop();;
+  if (file1 > file2) {
+    return 1;
+  } else if ( file1 < file2) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+function loadStories() {
+  req.keys().sort(sortByFileName).forEach(fileName => req(fileName))
+}
+
+configure(loadStories, module);
