bd9f45ada4441cbc5d982802adee38c90bb6374b
Joe Fehrman <joseph.fehrman@gmail.com>
2020-09-30 16:41:12 -0400

    Updating the url to pull data and the users list.

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,12 +1,5 @@
 // eslint-disable-next-line
-export const pullRequestUrl =
-  'https://api.github.com/search/issues?q=-label:invalid+created:2018-09-30T10%3A00%3A00%2B00%3A00..2018-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:{user}&per_page=300';
+const year = 2020;
+export const pullRequestUrl = `https://api.github.com/search/issues?q=-label:invalid+created:${year}-09-30T10%3A00%3A00%2B00%3A00..${year}-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:{user}&per_page=300`;
 
-export const users = [
-  'jfehrman',
-  'chalb500',
-  'protofitz',
-  'jlarkin42',
-  'jmicucci',
-  'gerritbond',
-];
+export const users = ['jfehrman'];
