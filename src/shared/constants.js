0e266f7668a21f9f94128d7f3e785c25f5a81d01
Joe Fehrman <joseph.fehrman@gmail.com>
2020-09-30 19:01:27 -0400

    Fixing query for data.

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
 const year = 2020;
-export const pullRequestUrl = `https://api.github.com/search/issues?q=-label:invalid+created:${year}-09-30T10%3A00%3A00%2B00%3A00..${year}-11-01T12%3A00%3A00%2B00%3A00+type:pr+is:public+author:{user}&per_page=300`;
+export const pullRequestUrl = `https://api.github.com/search/issues?q=-label:invalid+created:${year}-10-01T00%3A00%3A00%2B00%3A00..${year}-11-01T00%3A00%3A00%2B00%3A00+type:pr+is:public+author:{user}&per_page=300`;
 
 export const users = ['jfehrman'];
